76bd203b7181481c00c09f06eda71d1047d01f0d
TemPattrakorn <74557510+TemPattrakorn@users.noreply.github.com>
2023-09-14 10:37:26 +0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environment variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected to MongoDB")
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// HTTP REQUEST LISTENER
+	http.HandleFunc("/post", formSubmitHandler(client))
+	err2 := http.ListenAndServe(":8080", nil)
+	if err2 != nil {
+		panic(err2)
+	}
+
+}
+
+type MyDocument struct {
+	ItemName    string `bson:"item-name"`
+	Faculty     string `bson:"faculty"`
+	TeacherName string `bson:"teacher-name"`
+}
+
+func insertDocument(client *mongo.Client, document MyDocument) error {
+	// Get a handle to the target database and collection
+	collection := client.Database("senior_project").Collection("durable_goods")
+
+	// Insert the document
+	_, err := collection.InsertOne(context.Background(), document)
+	return err
+}
+
+//find
+/*
+func
+*/
+
+func formSubmitHandler(client *mongo.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			var doc MyDocument
+			err := r.ParseForm()
+			// Read the request body
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, "Error reading request body", http.StatusInternalServerError)
+				return
+			}
+
+			doc.ItemName = r.FormValue("item-name")
+			doc.Faculty = r.FormValue("faculty")
+			doc.TeacherName = r.FormValue("teacher-name")
+			// INSERT THE DOCUMENT TO MONGODB
+			err = insertDocument(client, doc)
+			// Print the received data to the server console
+			fmt.Println("Received data:", string(body))
+
+			// Respond with a confirmation message
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("Data received successfully"))
+		} else {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		}
+	}
+}
